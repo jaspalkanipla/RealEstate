4b13561664ca5287f163bfe13ded0555c126c20d
jaspal <dcas2325jaspal@kuk.ac.in>
2024-06-01 14:01:32 +0530

    react is ready

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div>
+      jaspal
+      <h1 className="text-2xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl  font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
